70baff91cf735c9b846c89c0d751cc5eab9bdf09
c.klewar <c.klewar@f5.com>
2024-02-01 12:38:30 +0100

    update examples

--- examples/three_node_cluster_single_nic_existing_vpc_new_subnet/main.tf
+++ examples/three_node_cluster_single_nic_existing_vpc_new_subnet/main.tf
@@ -22,7 +22,6 @@ module "f5xc_aws_cloud_ce_three_node_single_nic_new_vpc_new_subnet" {
   f5xc_aws_region       = var.f5xc_aws_region
   f5xc_token_name       = format("%s-%s-%s", var.project_prefix, var.f5xc_cluster_name, var.project_suffix)
   f5xc_cluster_name     = format("%s-%s-%s", var.project_prefix, var.f5xc_cluster_name, var.project_suffix)
-  aws_existing_vpc_id   = var.aws_existing_vpc_id
   f5xc_cluster_labels   = {}
   f5xc_aws_vpc_az_nodes = {
     node0 = {
@@ -41,6 +40,7 @@ module "f5xc_aws_cloud_ce_three_node_single_nic_new_vpc_new_subnet" {
   f5xc_ce_gateway_type                 = var.f5xc_ce_gateway_type
   f5xc_cluster_latitude                = var.f5xc_cluster_latitude
   f5xc_cluster_longitude               = var.f5xc_cluster_longitude
+  aws_existing_vpc_id                  = var.aws_existing_vpc_id
   aws_security_group_rules_slo_egress  = []
   aws_security_group_rules_slo_ingress = []
   ssh_public_key                       = file(var.ssh_public_key_file)
