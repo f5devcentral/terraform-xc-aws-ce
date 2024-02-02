0fe0b947f2a30c1e3d0324025cedba47a4012b8a
c.klewar <c.klewar@f5.com>
2024-02-02 16:57:53 +0100

    add appstack v1

---
+++ examples/three_node_cluster_appstack_new_vpc_new_subnet/main.tf
@@ -0,0 +1,55 @@
+locals {
+  aws_availability_zone_node0 = format("%s%s", var.f5xc_aws_region, var.f5xc_aws_availability_zone_node0)
+  aws_availability_zone_node1 = format("%s%s", var.f5xc_aws_region, var.f5xc_aws_availability_zone_node1)
+  aws_availability_zone_node2 = format("%s%s", var.f5xc_aws_region, var.f5xc_aws_availability_zone_node2)
+  custom_tags                 = {
+    Owner         = var.owner
+    f5xc-tenant   = var.f5xc_tenant
+    f5xc-template = "f5xc_aws_cloud_ce_three_node_appstack_single_nic_new_vpc_new_subnet"
+  }
+}
+
+module "f5xc_aws_cloud_ce_three_node_appstack_single_nic_new_vpc_new_subnet" {
+  source                = "../../modules/f5xc/ce/appstack/aws"
+  owner_tag             = var.owner
+  is_sensitive          = false
+  has_public_ip         = true
+  f5xc_tenant           = var.f5xc_tenant
+  f5xc_api_url          = var.f5xc_api_url
+  f5xc_api_token        = var.f5xc_api_token
+  f5xc_namespace        = var.f5xc_namespace
+  f5xc_aws_region       = var.f5xc_aws_region
+  f5xc_token_name       = format("%s-%s-%s", var.project_prefix, var.f5xc_cluster_name, var.project_suffix)
+  f5xc_cluster_name     = format("%s-%s-%s", var.project_prefix, var.f5xc_cluster_name, var.project_suffix)
+  f5xc_cluster_labels   = {}
+  f5xc_aws_vpc_az_nodes = {
+    node0 = {
+      f5xc_aws_vpc_slo_subnet = var.f5xc_aws_vpc_slo_subnet_node0
+      f5xc_aws_vpc_az_name    = local.aws_availability_zone_node0
+    }
+    node1 = {
+      f5xc_aws_vpc_slo_subnet = var.f5xc_aws_vpc_slo_subnet_node1
+      f5xc_aws_vpc_az_name    = local.aws_availability_zone_node1
+    }
+    node2 = {
+      f5xc_aws_vpc_slo_subnet = var.f5xc_aws_vpc_slo_subnet_node2
+      f5xc_aws_vpc_az_name    = local.aws_availability_zone_node2
+    }
+  }
+  f5xc_ce_gateway_type                 = var.f5xc_ce_gateway_type
+  f5xc_cluster_latitude                = var.f5xc_cluster_latitude
+  f5xc_cluster_longitude               = var.f5xc_cluster_longitude
+  aws_vpc_cidr_block                   = var.aws_vpc_cidr_block
+  aws_security_group_rules_slo_egress  = []
+  aws_security_group_rules_slo_ingress = []
+  ssh_public_key                       = file(var.ssh_public_key_file)
+  providers                            = {
+    aws      = aws.default
+    restapi  = restapi.default
+    volterra = volterra.default
+  }
+}
+
+output "f5xc_aws_cloud_ce_three_node_appstack_single_nic_new_vpc_new_subnet" {
+  value = module.f5xc_aws_cloud_ce_three_node_appstack_single_nic_new_vpc_new_subnet
+}
