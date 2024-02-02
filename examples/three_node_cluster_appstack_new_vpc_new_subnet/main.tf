b29214cc456daccfadfc9f24a03ed5b824d19b38
c.klewar <c.klewar@f5.com>
2024-02-02 16:20:55 +0100

    add appstack v1

--- examples/three_node_cluster_appstack_new_vpc_new_subnet/main.tf
+++ examples/three_node_cluster_appstack_new_vpc_new_subnet/main.tf
@@ -10,10 +10,9 @@ locals {
 }
 
 module "f5xc_aws_cloud_ce_three_node_appstack_single_nic_new_vpc_new_subnet" {
-  source                = "../../modules/f5xc/ce/"
+  source                = "../../modules/f5xc/ce/appstack/aws"
   owner_tag             = var.owner
   is_sensitive          = false
-  has_public_ip         = true
   f5xc_tenant           = var.f5xc_tenant
   f5xc_api_url          = var.f5xc_api_url
   f5xc_api_token        = var.f5xc_api_token
@@ -45,7 +44,6 @@ module "f5xc_aws_cloud_ce_three_node_appstack_single_nic_new_vpc_new_subnet" {
   ssh_public_key                       = file(var.ssh_public_key_file)
   providers                            = {
     aws      = aws.default
-    restapi  = restapi.default
     volterra = volterra.default
   }
 }
