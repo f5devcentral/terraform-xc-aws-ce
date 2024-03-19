682b083958bfae8afd647384358b8c75670e7753
c.klewar <c.klewar@f5.com>
2024-03-19 01:09:30 +0100

    remove mastercard rest api provider

---
+++ examples/single_node_single_nic_new_vpc_new_subnet_jframe_l7_osm/README.md
@@ -0,0 +1,26 @@
+# Example Single node single NIC new VPC new subnet with Offline Survivability Mode, L3 Mode Enhanced Performance and Jumbo Frame 
+
+This example instantiates:
+
+- Single node CE cluster with Offline Survivability Mode, L3 Mode Enhanced Performance with jumbo frame enabled
+- AWS SLO interface
+- AWS VPC
+- AWS SLO interface subnet
+- AWS route tables
+- AWS Internet Gateway
+- Assign AWS EIP to SLO
+
+## Topology
+
+![Single_node_single_nic_new_vpc_new_subnet_jframe_l7_osm](../../images/aws_1n.png)
+
+# Usage
+
+- Export AWS_ACCESS_KEY with: `export AWS_ACCESS_KEY_ID="aws access key id"`
+- Export AWS_SECRET_ACCESS_KEY with: `export AWS_SECRET_ACCESS_KEY=aws secret access key"`
+- Export F5 XC API certificate password with: `export VES_P12_PASSWORD="p12 password"`
+- Export F5 XC API token with: `export TF_VAR_f5xc_api_token="f5 xc api token"`
+- Edit `terraform.tfvars` file to align with your environment
+- Copy F5XC API certificate file obtained in installation step into example directory
+- Initialize with: `terraform init`, optionally run `terraform plan`
+- Apply with: `terraform apply -auto-approve` or destroy with: `terraform destroy -auto-approve`
---
+++ examples/single_node_single_nic_new_vpc_new_subnet_jframe_l7_osm/variables.tf
@@ -0,0 +1,102 @@
+variable "project_prefix" {
+  type        = string
+  description = "prefix string put in front of string"
+}
+
+variable "project_suffix" {
+  type        = string
+  description = "prefix string put at the end of string"
+}
+
+variable "f5xc_api_p12_file" {
+  description = "F5 XC API certificate file"
+  type        = string
+}
+
+variable "f5xc_api_p12_cert_password" {
+  description = "F5 XC API certificate file password"
+  type        = string
+  default     = ""
+}
+
+variable "f5xc_api_url" {
+  description = "F5 XC API URL"
+  type        = string
+}
+
+variable "f5xc_api_token" {
+  description = "F5 XC API token"
+  type        = string
+}
+
+variable "f5xc_tenant" {
+  description = "F5 XC Tenant name"
+  type        = string
+}
+
+variable "f5xc_namespace" {
+  description = "F5 XC namespace name"
+  type        = string
+}
+
+variable "f5xc_aws_region" {
+  description = "AWS region name"
+  type        = string
+}
+
+variable "f5xc_aws_availability_zone" {
+  description = "AWS availability zone name"
+  type        = string
+}
+
+variable "f5xc_cluster_name" {
+  description = "F5 XC CE cluster name"
+  type        = string
+}
+
+variable "owner" {
+  description = "AWS tag owner email address"
+  type        = string
+}
+
+variable "aws_vpc_cidr_block" {
+  description = "AWS VPC CIDR block"
+  type        = string
+}
+
+variable "f5xc_aws_vpc_slo_subnet_node0" {
+  description = "F5 XC CE SLO interface subnet prefix"
+  type        = string
+}
+
+variable "f5xc_ce_gateway_type" {
+  description = "F5 XC CE gateway type"
+  type        = string
+  default     = "ingress_gateway"
+}
+
+variable "f5xc_cluster_latitude" {
+  description = "F5 XC CE geo latitude"
+  type        = number
+  default     = -73.935242
+}
+
+variable "f5xc_cluster_longitude" {
+  description = "F5 XC CE geo longitude"
+  type        = number
+  default     = 40.730610
+}
+
+variable "f5xc_ce_performance_enhancement_mode" {
+  type = object({
+    perf_mode_l7_enhanced = bool
+    perf_mode_l3_enhanced = optional(object({
+      jumbo_frame_enabled = bool
+    }))
+  })
+}
+
+variable "ssh_public_key_file" {
+  description = "path to ssh public key file"
+  type        = string
+}
